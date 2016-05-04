3123f4fac9a61c63ceaf0a5a9cc720808e5b754d
Ash Furrow <ash@ashfurrow.com>
2016-05-04 13:18:34 -0400

    First attempt at 1.1.

---
+++ Part 1/1. Category- The Essence of Composition.playground/Contents.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+// https://bartoszmilewski.com/2014/11/04/category-the-essence-of-composition/
+
+//Implement, as best as you can, the identity function in your favorite language (or the second favorite, if your favorite language happens to be Haskell).
+
+func id<T, U>(closure: T -> U) -> T -> U {
+    return closure
+}
+
+func test (lhs: Int, rhs: Int) -> Void { }
+id(test)
+
+//Implement the composition function in your favorite language. It takes two functions as arguments and returns a function that is their composition.
+infix operator ◦ { associativity left }
+func ◦<A, B, C>(f: A -> B, g: B -> C) -> A -> C {
+    return { a in
+        g(f(a))
+    }
+}
+
+//Write a program that tries to test that your composition function respects identity.
+//f . id == f
+//id . f == f
+
+func f(operand: Int) -> Int {
+    return operand + 1
+}
+
+//f ◦ id // Seems to be a limiation of Swift's type system and generics.
+//id ◦ f // Seems to be a limiation of Swift's type system and generics.
+
+//Is the world-wide web a category in any sense? Are links morphisms?
+//Is Facebook a category, with people as objects and friendships as morphisms?
+//When is a directed graph a category?
